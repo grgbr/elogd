000070069b7dd71ea07a4dd8a908de04a50251db
Gregor Boirie <gregor.boirie@free.fr>
2024-02-19 08:01:31 +0100

    refine top-level build logic

--- Makefile
+++ Makefile
@@ -1,7 +1,20 @@
-PACKAGE       := elogd
-EXTRA_CFLAGS  := -O2 -DNDEBUG
-EXTRA_LDFLAGS := -O2
+override PACKAGE := elogd
+override VERSION := 1.0
+EXTRA_CFLAGS     := -O2 -DNDEBUG
+EXTRA_LDFLAGS    := $(EXTRA_CFLAGS)
 
-export EXTRA_CFLAGS EXTRA_LDFLAGS
+export VERSION EXTRA_CFLAGS EXTRA_LDFLAGS
+
+ifeq ($(strip $(EBUILDDIR)),)
+ifneq ($(realpath ebuild/main.mk),)
+EBUILDDIR := $(realpath ebuild)
+else  # ($(realpath ebuild/main.mk),)
+EBUILDDIR := $(realpath /usr/share/ebuild)
+endif # !($(realpath ebuild/main.mk),)
+endif # ($(strip $(EBUILDDIR)),)
+
+ifeq ($(realpath $(EBUILDDIR)/main.mk),)
+$(error '$(EBUILDDIR)': no valid eBuild install found !)
+endif # ($(realpath $(EBUILDDIR)/main.mk),)
 
 include $(EBUILDDIR)/main.mk
--- ebuild.mk
+++ ebuild.mk
@@ -1,10 +1,24 @@
-config-in                  := Config.in
+################################################################################
+# SPDX-License-Identifier: LGPL-3.0-only
+#
+# This file is part of eLogd.
+# Copyright (C) 2022-2024 Grégor Boirie <gregor.boirie@free.fr>
+################################################################################
 
-common-cflags              := -Wall -Wextra -Wformat=2 $(EXTRA_CFLAGS)
+config-in     := Config.in
+config-h      := $(PACKAGE)/config.h
 
-bins                       += elogd
-elogd-objs                 := elogd.o
-elogd-cflags               := -Wall -Wextra -Wformat=2 $(EXTRA_CFLAGS)
-elogd-ldflags              := $(EXTRA_LDFLAGS)
-elogd-pkgconf              := libelog libenbox libutils
-elogd-path                 := $(SBINDIR)/elogd
+common-cflags := -Wall -Wextra -Wformat=2 $(EXTRA_CFLAGS)
+
+bins          := elogd
+elogd-objs    := elogd.o
+elogd-cflags  := -Wall -Wextra -Wformat=2 $(EXTRA_CFLAGS)
+elogd-ldflags := $(EXTRA_LDFLAGS)
+elogd-pkgconf := libelog libenbox libutils
+elogd-path    := $(SBINDIR)/elogd
+
+################################################################################
+# Source code tags generation
+################################################################################
+
+tagfiles := $(shell find $(CURDIR) -type f)
